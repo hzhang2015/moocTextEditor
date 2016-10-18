2546c37dd6e4253b3ac608fe075012886bbe412a
Hong Zhang <Hong.Zhang@teamaol.com>
2016-10-18 17:41:13 -0400

    more

--- src/spelling/DictionaryBST.java
+++ src/spelling/DictionaryBST.java
@@ -1,5 +1,6 @@
 package spelling;
 
+import java.util.Objects;
 import java.util.TreeSet;
 
 /**
@@ -10,9 +11,9 @@ public class DictionaryBST implements Dictionary
 {
    private TreeSet<String> dict;
 	
-    // TODO: Implement the dictionary interface using a TreeSet.  
- 	// You'll need a constructor here
-	
+    public DictionaryBST(){
+        dict = new TreeSet<>();
+    }
     
     /** Add this word to the dictionary.  Convert it to lowercase first
      * for the assignment requirements.
@@ -20,7 +21,12 @@ public class DictionaryBST implements Dictionary
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
+        Objects.requireNonNull(word);
+        String lowerCaseWord = word.toLowerCase();
+        if (!dict.contains(lowerCaseWord)){
+            dict.add(lowerCaseWord);
+            return true;
+        }
         return false;
     }
 
@@ -28,14 +34,13 @@ public class DictionaryBST implements Dictionary
     /** Return the number of words in the dictionary */
     public int size()
     {
-    	// TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-    	//TODO: Implement this method
-        return false;
+        Objects.requireNonNull(s);
+        return dict.contains(s.toLowerCase());
     }
 
 }
--- src/spelling/DictionaryLL.java
+++ src/spelling/DictionaryLL.java
@@ -1,6 +1,7 @@
 package spelling;
 
 import java.util.LinkedList;
+import java.util.Objects;
 
 /**
  * A class that implements the Dictionary interface using a LinkedList
@@ -11,8 +12,9 @@ public class DictionaryLL implements Dictionary
 
 	private LinkedList<String> dict;
 	
-    // TODO: Add a constructor
-
+    public DictionaryLL(){
+        dict = new LinkedList<>();
+    }
 
     /** Add this word to the dictionary.  Convert it to lowercase first
      * for the assignment requirements.
@@ -20,7 +22,12 @@ public class DictionaryLL implements Dictionary
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
+        Objects.requireNonNull(word);
+        String lowerCaseWord = word.toLowerCase();
+        if (!dict.contains(lowerCaseWord)){
+            dict.add(lowerCaseWord);
+            return true;
+        }
         return false;
     }
 
@@ -28,14 +35,13 @@ public class DictionaryLL implements Dictionary
     /** Return the number of words in the dictionary */
     public int size()
     {
-        // TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-        //TODO: Implement this method
-        return false;
+        Objects.requireNonNull(s);
+        return dict.contains(s.toLowerCase());
     }
 
     
