957e942ff2615ae420eab41b09cf0cdff21ca10d
Hong Zhang <Hong.Zhang@teamaol.com>
2016-10-29 15:58:04 -0400

    more code

--- src/spelling/NearbyWords.java
+++ src/spelling/NearbyWords.java
@@ -3,10 +3,7 @@
  */
 package spelling;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 
 /**
@@ -76,7 +73,17 @@ public class NearbyWords implements SpellingSuggest {
 	 * @return
 	 */
 	public void insertions(String s, List<String> currentList, boolean wordsOnly ) {
-		// TODO: Implement this method  
+		for (int index =0; index <= s.length(); index++){
+			for (int charCode = (int)'a'; charCode <= (int)'z'; charCode ++){
+				StringBuffer sb = new StringBuffer(s);
+				sb.insert(index, (char) charCode);
+				if(!currentList.contains(sb.toString()) &&
+						(!wordsOnly||dict.isWord(sb.toString())) &&
+						!s.equals(sb.toString())) {
+					currentList.add(sb.toString());
+				}
+			}
+		}
 	}
 
 	/** Add to the currentList Strings that are one character deletion away
@@ -87,7 +94,16 @@ public class NearbyWords implements SpellingSuggest {
 	 * @return
 	 */
 	public void deletions(String s, List<String> currentList, boolean wordsOnly ) {
-		// TODO: Implement this method
+		for (int index =0; index < s.length(); index++){
+			StringBuffer sb = new StringBuffer(s);
+			sb.deleteCharAt(index);
+
+			if(!currentList.contains(sb.toString()) &&
+					(!wordsOnly||dict.isWord(sb.toString())) &&
+					!s.equals(sb.toString())) {
+				currentList.add(sb.toString());
+			}
+		}
 	}
 
 	/** Add to the currentList Strings that are one character deletion away
@@ -100,38 +116,51 @@ public class NearbyWords implements SpellingSuggest {
 	public List<String> suggestions(String word, int numSuggestions) {
 
 		// initial variables
-		List<String> queue = new LinkedList<String>();     // String to explore
+		Queue<String> queue = new LinkedList<String>();     // String to explore
 		HashSet<String> visited = new HashSet<String>();   // to avoid exploring the same  
 														   // string multiple times
 		List<String> retList = new LinkedList<String>();   // words to return
-		 
-		
+
 		// insert first node
 		queue.add(word);
 		visited.add(word);
 					
-		// TODO: Implement the remainder of this method, see assignment for algorithm
-		
+		while (!queue.isEmpty() && numSuggestions > 0){
+			for (String s: distanceOne(queue.poll(), false)) {
+				if (numSuggestions == 0)
+					break;
+				if (! visited.contains(s)){
+					visited.add(s);
+					queue.offer(s);
+					if (dict.isWord(s)){
+						retList.add(s);
+						numSuggestions--;
+					}
+				}
+			}
+			if (visited.size() == THRESHOLD && retList.size() > 0)
+				break;
+		}
 		return retList;
 
 	}	
 
    public static void main(String[] args) {
-	   /* basic testing code to get started
-	   String word = "i";
-	   // Pass NearbyWords any Dictionary implementation you prefer
-	   Dictionary d = new DictionaryHashSet();
-	   DictionaryLoader.loadDictionary(d, "data/dict.txt");
-	   NearbyWords w = new NearbyWords(d);
-	   List<String> l = w.distanceOne(word, true);
-	   System.out.println("One away word Strings for for \""+word+"\" are:");
-	   System.out.println(l+"\n");
+//	   // basic testing code to get started
+//	   String word = "lteo";
+//	   // Pass NearbyWords any Dictionary implementation you prefer
+//	   Dictionary d = new DictionaryHashSet();
+//	   DictionaryLoader.loadDictionary(d, "data/dict.txt");
+//	   NearbyWords w = new NearbyWords(d);
+//	   List<String> l = w.distanceOne(word, true);
+//	   System.out.println("One away word Strings for for \""+word+"\" are:");
+//	   System.out.println(l+"\n");
+//
+//	   word = "kangaro";
+//	   List<String> suggest = w.suggestions(word, 10);
+//	   System.out.println("Spelling Suggestions for \""+word+"\" are:");
+//	   System.out.println(suggest);
 
-	   word = "tailo";
-	   List<String> suggest = w.suggestions(word, 10);
-	   System.out.println("Spelling Suggestions for \""+word+"\" are:");
-	   System.out.println(suggest);
-	   */
    }
 
 }
